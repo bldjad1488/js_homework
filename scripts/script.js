88466e449205d74eb74105f0100308141dbfe120
Maxim Prokopenko <luciusrichter2007@gmail.com>
2020-02-18 20:32:31 +1000

    готово

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,16 @@
 'esversion: 6';
 
+
+
 const   startButton    = document.querySelector('.start-button'),
         firstScreen    = document.querySelector('.first-screen'),
         mainForm       = document.querySelector('.main-form'),
         formCalculate  = document.querySelector('.form-calculate'),
         endButton      = document.querySelector('.end-button'),
-        total          = document.querySelector('.total');
+        total          = document.querySelector('.total'),
+        fastRange      = document.querySelector('.fast-range');
+
+
 
 function showElem(elem) {
     elem.style.display = 'block';
@@ -15,6 +20,11 @@ function hideElem(elem) {
     elem.style.display = 'none';
 }
 
+function handlerCallBackForm(event) {
+    const target = event.target;
+    
+    target.checked ? showElem(fastRange) : hideElem(fastRange);
+}
 
 
 
@@ -23,3 +33,14 @@ startButton.addEventListener('click', function() {
     hideElem(firstScreen);
 });
 
+endButton.addEventListener('click', function() {
+    for (const elem of formCalculate.elements) {
+        if (elem.tagName === 'FIELDSET') {
+            hideElem(elem);
+        }
+
+        showElem(total);
+    }
+});
+
+formCalculate.addEventListener('change', handlerCallBackForm);
